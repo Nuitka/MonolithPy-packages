b16b22751e18f75e9a2749152225be407328563c
Maxwell Dreytser <admin@mdtech.us>
2026-01-12 16:09:18 -0500

    Add countourpy on windows script

---
+++ packages/mp313-windows/contourpy/build.py
@@ -0,0 +1,37 @@
+import __mp__
+import glob
+import shutil
+import sys
+import os
+import setuptools.build_meta
+from tempfile import TemporaryDirectory
+
+from wheel.wheelfile import WheelFile
+
+
+def run(wheel_directory):
+    __mp__.setup_compiler_env()
+
+    env = os.environ.copy()
+    job_args = []
+    if "MP_JOBS" in env:
+        job_args += ["-Ccompile-args=-j" + env["MP_JOBS"]]
+    __mp__.run(sys.executable, "-m", "build", "-w", "--no-isolation", *job_args)
+
+    wheel_location = glob.glob(os.path.join("dist", "contourpy-*.whl"))[0]
+
+    wheel_files = []
+    with TemporaryDirectory() as tmpdir:
+        with WheelFile(wheel_location) as wf:
+            for filename in wf.namelist():
+                wheel_files.append(filename)
+                wf.extract(filename, tmpdir)
+        __mp__.analyze_and_rename_library_symbols(tmpdir,
+                                                  "contourpy")
+        with WheelFile(wheel_location, 'w') as wf:
+            for filename in wheel_files:
+                wf.write(os.path.join(tmpdir, filename), filename)
+
+    wheel_name = os.path.basename(wheel_location)
+    shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
+    return os.path.join(wheel_directory, wheel_name)
