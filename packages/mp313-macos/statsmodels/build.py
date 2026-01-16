e353c8fcc3b24e2757d3208661ed6c5ac65717d1
Maxwell Dreytser <admin@mdtech.us>
2026-01-15 21:17:28 -0500

    Add statsmodels script to ignore docstring manipulation.

---
+++ packages/mp313-macos/statsmodels/build.py
@@ -0,0 +1,19 @@
+import __mp__
+import glob
+import shutil
+import sys
+import os
+import setuptools.build_meta
+
+
+def run(wheel_directory):
+    __mp__.run_with_output("patch", "-t", "-p1", "-i",
+                              os.path.join(os.path.dirname(__file__), "statsmodels-static-patch.patch"))
+
+    __mp__.run_with_output(sys.executable, "-m", "build", "-w", "--no-isolation")
+
+    wheel_location = glob.glob(os.path.join("dist", "statsmodels-*.whl"))[0]
+
+    wheel_name = os.path.basename(wheel_location)
+    shutil.copy(wheel_location, os.path.join(wheel_directory, wheel_name))
+    return os.path.join(wheel_directory, wheel_name)
