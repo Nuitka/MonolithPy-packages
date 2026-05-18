c2cf80993220e87cbbfda9681460abaa60ef1236
Maxwell Dreytser <admin@mdtech.us>
2026-05-18 01:22:14 -0400

    zlib: Use cmake for build

--- dependencies/mp314-windows/zlib/build.py
+++ dependencies/mp314-windows/zlib/build.py
@@ -11,16 +11,28 @@ def run(wheel_directory):
 
     __mp__.setup_compiler_env()
 
-    __mp__.auto_patch_build(os.path.join(src_dir, "win32"))
+    __mp__.auto_patch_build(src_dir)
 
-    os.chdir(src_dir)
+    build_dir = os.path.join(src_dir, "build")
+    os.mkdir(build_dir)
+    os.chdir(build_dir)
 
-    __mp__.nmake("/f", "win32/Makefile.msc")
+    os.environ["PATH"] = os.path.dirname(__mp__.find_build_tool_exe("ninja", "ninja.exe")) + os.pathsep + os.environ["PATH"]
+    __mp__.run_build_tool_exe("cmake", "cmake.exe", "-G", "Ninja",
+                              "-DCMAKE_BUILD_TYPE=Release",
+                              "-DBUILD_SHARED_LIBS=OFF",
+                              src_dir)
+    __mp__.run_build_tool_exe("ninja", "ninja.exe", "zlibstatic")
+
+    # Normalize output name: zlib's CMake build produces zlibstatic.lib;
+    # downstream packages expect zlib.lib.
+    shutil.copy(os.path.join(build_dir, "zlibstatic.lib"), os.path.join(build_dir, "zlib.lib"))
 
     result_wheel = os.path.join(wheel_directory, __mp__.get_wheel_name("mpy_dep_zlib", "1.2.12"))
     with WheelFile(result_wheel, 'w') as w:
         __mp__.add_wheel_manifest(w, "mpy-dep-zlib", "1.2.12")
-        __mp__.add_wheel_dep_libs(w, "zlib", os.path.join(src_dir, "zlib.lib"))
+        __mp__.add_wheel_dep_libs(w, "zlib", os.path.join(build_dir, "zlib.lib"))
         __mp__.add_wheel_dep_include(w, "zlib", os.path.join(src_dir, "*.h"))
+        __mp__.add_wheel_dep_include(w, "zlib", os.path.join(build_dir, "*.h"))
 
     return result_wheel
